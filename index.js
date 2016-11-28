c10ffe8ae0da78ac676acd2563bab0c819ba64bd
Christoph <christoph.khouri@gmail.com>
2016-11-28 18:48:08 -0500

    fixing dates

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
 
+/*
+TestIntent what is for lunch
+TestIntent what's for lunch
+
+HelloWorld - arn:aws:lambda:us-east-1:452114835050:function:HelloWorld
+Latin-lunch - arn:aws:lambda:us-east-1:452114835050:function:latin-lunch
+{
+  "intents": [
+      {
+      "intent": "TestIntent"
+    },
+    {
+      "intent": "GetLunchIntent",
+      "slots": [
+        {
+          "name": "Date",
+          "type": "AMAZON.DATE"
+        }
+      ]
+    }
+  ]
+}
+
+
+*/
+
 var moment = require('moment');
 var request = require('request');
 var cheerio = require('cheerio');
-
-if ( parseInt( moment().format("HH") ) < 12) {
-    var current_day = moment().format("YYYY-MM-DD");
-    var date_id = moment().format("MDYY");
-    var when = "today";
-} else {
-    var current_day = moment(new Date()).add(1, 'days').format("YYYY-MM-DD");
-    var date_id = moment(new Date()).add(1, 'days').format("MDYY");
-    var when = "tomorrow";
-}
-
 
 var url = "http://www.myschooldining.com/Latin/calendarWeek";
 var headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
     'Content-Type' : 'application/x-www-form-urlencoded'
 };
-var form = {
-    current_day: current_day,
-    adj: 0
-};
 
 
 exports.handler = function (event, context) {
     try {
-        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
-        console.log(event);
-        console.log(context);
+        // console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+        console.log("Event: ");
+            console.log(event);
+        console.log("context: ")
+            console.log(context);
 
         /*
-        if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.[unique-value-here]") {
+        if (event.session.application.applicationId !== "amzn1.ask.skill.8f4a426c-1ff3-45a5-aa58-8994da82c293") {
              context.fail("Invalid Application ID");
          }
         */
@@ -66,8 +79,7 @@ exports.handler = function (event, context) {
  * Called when the session starts.
  */
 function onSessionStarted(sessionStartedRequest, session) {
-    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
-                + ", sessionId=" + session.sessionId);
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId + ", sessionId=" + session.sessionId);
 }
 
 /**
@@ -86,9 +98,10 @@ function onIntent(intentRequest, session, callback) {
     var intent = intentRequest.intent,
         intentName = intentRequest.intent.name;
 
+    console.log("Called intent: " + intentName)
     // Dispatch to your skill's intent handlers
-    if ("TestIntent" === intentName) {
-        testResponse(intent, session, callback);
+    if ("GetLunchIntent" === intentName) {
+        getLunchResponse(intent, session, callback);
     }  else {
         throw "Invalid intent";
     }
@@ -109,9 +122,7 @@ function getWelcomeResponse(callback) {
     // If we wanted to initialize the session to have some attributes we could add those here.
     var sessionAttributes = {};
     var cardTitle = "Welcome";
-    var speechOutput = "Welcome to Latin Lunch.  Ask me what's for lunch."
-    // If the user either does not reply to the welcome message or says something that is not
-    // understood, they will be prompted again with this text.
+    var speechOutput = "Welcome to Charlotte Latin's lunch menu.  Ask me what's for lunch, or what's for lunch tomorrow."
     var repromptText = "Are you not hungry?";
     var shouldEndSession = false;
 
@@ -119,12 +130,45 @@ function getWelcomeResponse(callback) {
 }
 
 
-function testResponse(intent, session, callback) {
+function getLunchResponse(intent, session, callback) {
     var cardTitle = intent.name;
     var repromptText = null;
     var sessionAttributes = {};
     var shouldEndSession = true;
     var speechOutput = "Latin lunch";
+
+    var d = new Date();
+    var requestedDate = d.toISOString();
+    
+    if (intent.slots.Date.value) {
+        console.log("Date passed in as: " + intent.slots.Date.value)
+        requestedDate = intent.slots.Date.value;
+        var current_day = moment(requestedDate).format("YYYY-MM-DD");
+        var date_id = moment(requestedDate).format("MDYY");
+        var when = moment(requestedDate).format("dddd");
+
+    } else {
+
+        console.log("Actual full date is: " + moment().format("MMMM Do YYYY, h:mm:ss a (HH)") )
+
+        if ( parseInt( moment().format("HH") ) < 12) {
+            var current_day = moment().format("YYYY-MM-DD");
+            var date_id = moment().format("MDYY");
+            var when = "today";
+        } else {
+            var current_day = moment(new Date()).add(1, 'days').format("YYYY-MM-DD");
+            var date_id = moment(new Date()).add(1, 'days').format("MDYY");
+            var when = moment(new Date()).add(1, 'days').format("dddd");//"tomorrow";
+        }
+
+        console.log("when:" + when + ", current_day:" + current_day + ", date_id:" + date_id)
+
+}
+
+    var form = {
+        current_day: current_day,
+        adj: 0
+    };
 
 
     request.post({ url: url, form: form, headers: headers }, function(err, resp, data) {
